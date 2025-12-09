a65d4ebeba0e821e4c3d5b378916774b872afc70
Leonardo Noronha Dornelles <leonardondornelles@gmail.com>
2025-12-09 18:49:25 -0300

    Adicionando prints do projeto

--- tf-poo/src/main/java/com/exemplo/App.java
+++ tf-poo/src/main/java/com/exemplo/App.java
@@ -1,60 +1,84 @@
 package com.exemplo;
 
-import com.exemplo.models.*;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.exemplo.models.Empresa;
+import com.exemplo.models.Estudante;
+import com.exemplo.models.Funcionario;
+import com.exemplo.models.GerenciadorUsuarios;
+import com.exemplo.models.Usuario;
 
 @SpringBootApplication
 public class App {
+
     public static void main(String[] args) {
-        Estudante e1 = new Estudante("Nicolas", "AA");
-        Estudante e2 = new Estudante("Leonardo", "BB");
-        Estudante e3 = new Estudante("Eduardo", "CC");
-        Estudante e4 = new Estudante("Rodrigo", "DD");
-        Estudante e5 = new Estudante("João", "EE");
-        Estudante e6 = new Estudante("Nicolas Azambuja", "FF");
-
-        Empresa em1 = new Empresa("Dell", "GG");
-        Empresa em2 = new Empresa("SAP", "HH");
-        Empresa em3 = new Empresa("HP", "II");
-
-        Funcionario f1 = new Funcionario("Karina", "JJ");
-        Funcionario f2 = new Funcionario("Giraffa", "KK");
-        Funcionario f3 = new Funcionario("Marlon", "LL");
-        Funcionario f4 = new Funcionario("Mrs Alest", "NN");
-
-        e1.registrarVeiculo("MM");
-        e2.registrarVeiculo("OO");
-        e3.registrarVeiculo("PP");
-        e5.registrarVeiculo("QQ");
-
-        em1.registrarVeiculo("XX");
-        em1.registrarVeiculo("SSSS");
-        em1.registrarVeiculo("OOOO");
-        em1.registrarVeiculo("LLLL");
-
-        em2.registrarVeiculo("JJJJJJ");
-        em2.registrarVeiculo("UUUUU");
-        em2.registrarVeiculo("BBBBB");
-        em2.registrarVeiculo("QQQQQQ");
-
-        f1.registrarVeiculo("UU");
-        f3.registrarVeiculo("VV");
-
-        GerenciadorUsuarios.gerenciadorUsuario.add(e1);
-        GerenciadorUsuarios.gerenciadorUsuario.add(e2);
-        GerenciadorUsuarios.gerenciadorUsuario.add(e3);
-        GerenciadorUsuarios.gerenciadorUsuario.add(e4);
-        GerenciadorUsuarios.gerenciadorUsuario.add(e5);
-        GerenciadorUsuarios.gerenciadorUsuario.add(e6);
-        GerenciadorUsuarios.gerenciadorUsuario.add(em1);
-        GerenciadorUsuarios.gerenciadorUsuario.add(em2);
-        GerenciadorUsuarios.gerenciadorUsuario.add(em3);
-        GerenciadorUsuarios.gerenciadorUsuario.add(f1);
-        GerenciadorUsuarios.gerenciadorUsuario.add(f2);
-        GerenciadorUsuarios.gerenciadorUsuario.add(f3);
-        GerenciadorUsuarios.gerenciadorUsuario.add(f4);
-
         SpringApplication.run(App.class, args);
+    }
+
+    /**
+     * Este método roda automaticamente assim que o Spring Boot termina de iniciar.
+     * É o lugar perfeito para carregar dados de teste.
+     */
+    @Bean
+    public CommandLineRunner carregarDadosIniciais() {
+        return (args) -> {
+            // --- ESTUDANTES ---
+            Estudante e1 = new Estudante("Nicolas", "21103040");
+            Estudante e2 = new Estudante("Leonardo Dornelles", "21180200");
+            Estudante e3 = new Estudante("Eduardo", "21200100");
+            Estudante e4 = new Estudante("Rodrigo", "21200200");
+            Estudante e5 = new Estudante("João", "21200300");
+            Estudante e6 = new Estudante("Nicolas Azambuja", "21200400");
+
+            // Registrando veículos dos estudantes
+            e1.registrarVeiculo("IJK-1920");
+            e2.registrarVeiculo("LND-2025");
+            e3.registrarVeiculo("EDU-8080");
+            e5.registrarVeiculo("JOA-9999");
+
+            // --- EMPRESAS ---
+            Empresa em1 = new Empresa("Dell Technologies", "99.999.001/0001-01");
+            Empresa em2 = new Empresa("SAP Labs", "88.888.002/0001-02");
+            Empresa em3 = new Empresa("HP Inc.", "77.777.003/0001-03");
+
+            // Registrando veículos das empresas
+            em1.registrarVeiculo("DEL-0001");
+            em1.registrarVeiculo("DEL-0002");
+            em1.registrarVeiculo("DEL-0003");
+            em1.registrarVeiculo("SER-VER1");
+
+            em2.registrarVeiculo("SAP-1001");
+            em2.registrarVeiculo("SAP-1002");
+            em2.registrarVeiculo("SAP-1003");
+            em2.registrarVeiculo("CLO-UD01");
+
+            // --- CRIANDO FUNCIONÁRIOS PUCRS ---
+            Funcionario f1 = new Funcionario("Karina", "FUNC-001");
+            Funcionario f2 = new Funcionario("Lucia Giraffa", "FUNC-002");
+            Funcionario f3 = new Funcionario("Marlon", "FUNC-003");
+            Funcionario f4 = new Funcionario("Mrs. Alest", "FUNC-004");
+
+            // Registrando veículos dos funcionários
+            f1.registrarVeiculo("PUC-1111");
+            f3.registrarVeiculo("PUC-3333");
+
+            
+            // Usando uma lista temporária para adicionar tudo de uma vez com um loop
+            List<Usuario> todosUsuarios = Arrays.asList(
+                e1, e2, e3, e4, e5, e6,
+                em1, em2, em3,
+                f1, f2, f3, f4
+            );
+
+            for (Usuario u : todosUsuarios) {
+                GerenciadorUsuarios.gerenciadorUsuario.add(u);
+            }
+        };
     }
 }
